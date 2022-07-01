a66d4607111fb416e7d07147bf93b55383696dee
Younis <younisrahman007@gmail.com>
2022-07-02 01:36:12 +0600

    input age

--- Basic/inputage.py
+++ Basic/inputage.py
@@ -1,1 +1,7 @@
 # 3. input age
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+year = (2022-age) + 100
+print("Hello " + name + " you will be 100 years old in " + str(year))

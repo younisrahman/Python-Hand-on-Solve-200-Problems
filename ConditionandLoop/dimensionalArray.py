2a37dc91c426e3c1900c295fae2f2ad6e9377158
Younis <younisrahman007@gmail.com>
2022-07-02 13:44:54 +0600

    row*col

--- ConditionandLoop/dimensionalArray.py
+++ ConditionandLoop/dimensionalArray.py
@@ -8,3 +8,11 @@
 # Input number of columns: 4
 # Output
 # [[0, 0, 0, 0], [0, 1, 2, 3], [0, 2, 4, 6]]
+
+row_num = int(input("Input number of rows: "))
+col_num = int(input("Input number of columns: "))
+multi_list = [[row*col for col in range(col_num)] for row in range(row_num)]
+# for row in range(row_num):
+#     for col in range(col_num):
+#         multi_list[row][col] = row*col
+print(multi_list)
